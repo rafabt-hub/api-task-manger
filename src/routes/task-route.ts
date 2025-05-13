d17d319fc1304d8cfdb1090750cbaee2c3c394f5
Rafael <rafaelbthomaz@gmail.com>
2025-05-13 15:08:54 -0300

    verify user

--- src/routes/task-route.ts
+++ src/routes/task-route.ts
@@ -3,11 +3,12 @@ import { Router } from "express"
 import { TaskController } from "@/controllers/task-controller"
 
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated"
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization"
 
 const taskRoutes = Router()
 const taskController = new TaskController()
 
-taskRoutes.use(ensureAuthenticated)
+taskRoutes.use(ensureAuthenticated, verifyUserAuthorization(["admin"]))
 taskRoutes.post("/", taskController.create)
 
 export { taskRoutes }
