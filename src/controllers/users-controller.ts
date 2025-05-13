eff8e326d36bc459a713d596422c26bc7b98844d
Rafael <rafaelbthomaz@gmail.com>
2025-05-13 13:26:53 -0300

    adding bcrypt + config prisma

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,8 +1,20 @@
-import { Request, Response } from "express";
+import { Request, Response } from "express"
+import { hash } from "bcrypt"
+import { z } from "zod"
 
 class UsersController {
-  create(request: Request, response: Response) {
-    return response.json({ message: "ok"})
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(2),
+      email: z.string().email(),
+      password: z.string().min(6),
+    })
+
+    const { name, email, password } = bodySchema.parse(request.body)
+
+    const hasedPassword = await hash(password, 8)
+
+    return response.json({ message: "ok", hasedPassword})
   }
 }
 
