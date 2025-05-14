2f446860262e6d75db5b2ebe2ebadfd517000f7c
Rafael <rafaelbthomaz@gmail.com>
2025-05-14 17:25:43 -0300

    fixing user test

--- src/tests/users-controllers.test.ts
+++ src/tests/users-controllers.test.ts
@@ -1,8 +1,15 @@
 import request from "supertest"
+import { prisma } from "@/database/prisma"
 
 import { app } from "@/app"
 
 describe("UsersController", () => {
+  let user_id: string
+
+  afterAll(async () => {
+    await prisma.user.delete({ where: {id: user_id} })
+  })
+
   it("should create a new user sucessfully", async () => {
     const response = await request(app).post("/users").send({
       name: "Test User",
@@ -13,5 +20,7 @@ describe("UsersController", () => {
     expect(response.status).toBe(201)
     expect(response.body).toHaveProperty("id")
     expect(response.body.name).toBe("Test User")
+
+    user_id = response.body.id
  })
 })
